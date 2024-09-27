0ffbe20738e2688b7640dc6ae44ecabb39ac6949
Max H. <10329648+8ear@users.noreply.github.com>
2024-09-27 08:34:38 +0200

    Update docker-entrypoint.sh
    Fix issues

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,28 +4,37 @@ set -e
 echo "Start Entrypoint ..."
 
 echo "Add variables for Azure..."
-# Add variables for Azure as ENV
+# Add variables for Azure cli as ENV
 AZURE_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID}
 AZURE_CLIENT_ID=${AZURE_CLIENT_ID}
-AZURE_SECRET=${AZURE_SECRET}
+AZURE_CLIENT_SECRET=${AZURE_CLIENT_SECRET}
 AZURE_TENANT_ID=${AZURE_TENANT_ID}
 # Add variables for Azure as secret -> secrets are more preferred
-[ -f /run/secrets/AZURE_SUBSCRIPTION_ID ] && AZURE_SUBSCRIPTION_ID=${cat /run/secrets/AZURE_SUBSCRIPTION_ID}
-[ -f /run/secrets/AZURE_CLIENT_ID ] && AZURE_CLIENT_ID=${cat /run/secrets/AZURE_CLIENT_ID}
-[ -f /run/secrets/AZURE_SECRET ] && AZURE_SECRET=${cat /run/secrets/AZURE_SECRET}
-[ -f /run/secrets/AZURE_TENANT_ID ] && AZURE_TENANT_ID=${cat /run/secrets/AZURE_TENANT_ID}
+[ -f /run/secrets/AZURE_SUBSCRIPTION_ID ] && export AZURE_SUBSCRIPTION_ID=${cat /run/secrets/AZURE_SUBSCRIPTION_ID}
+[ -f /run/secrets/AZURE_CLIENT_ID ] && export AZURE_CLIENT_ID=${cat /run/secrets/AZURE_CLIENT_ID}
+[ -f /run/secrets/AZURE_CLIENT_SECRET ] && export AZURE_CLIENT_SECRET=${cat /run/secrets/AZURE_CLIENT_SECRET}
+[ -f /run/secrets/AZURE_TENANT_ID ] && export AZURE_TENANT_ID=${cat /run/secrets/AZURE_TENANT_ID}
 
+# For Ansible AzureRM Inventory:
+# https://docs.ansible.com/ansible/latest/collections/azure/azcollection/azure_rm_inventory.html#notes
+# AZURE_SUBSCRIPTION_ID
+# AZURE_CLIENT_ID
+export AZURE_SECRET=${AZURE_CLIENT_SECRET}
+export AZURE_TENANT=${AZURE_TENANT_ID}
 
-# Create Azure credential file
-mkdir ~/.azure
-cat << EOF > ~/.azure/credentials
-[default]
-subscription_id=${AZURE_SUBSCRIPTION_ID}
-client_id=${AZURE_CLIENT_ID}
-secret=${AZURE_SECRET}
-tenant=${AZURE_TENANT_ID}
-EOF
-
+# Check if i can login to azure cli
+if [ -n "${AZURE_CLIENT_ID}"  ] && [ -n "${AZURE_CLIENT_SECRET}"  ] && [ -n "${AZURE_TENANT_ID}"  ] 
+then
+    echo
+    echo "Login to Azure CLI..."
+    az login --service-principal --username ${AZURE_CLIENT_ID} --password ${AZURE_CLIENT_SECRET} --tenant ${AZURE_TENANT_ID}
+    az account get-access-token
+    echo
+else
+    echo
+    echo "I cannot login to azure cli because of missing of one or all of the ENV vars: AZURE_CLIENT_ID AZURE_CLIENT_SECRET AZURE_TENANT_ID"
+    echo
+fi
 
 # Install requirements file if exists in ~/requirements.yml
 echo "Check if additional galaxies must be installed from ~/requirements.yml or ~/ansible/requirements.yml"
