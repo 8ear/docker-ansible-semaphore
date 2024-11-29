970b4fff10aab35ba109332b867f9dfc42f72e01
Max H. <10329648+8ear@users.noreply.github.com>
2024-11-29 10:39:57 +0100

    Update
    - Fix #15 
    - Fix azure module errror with msgraph
    - Add symlink for .azure and .ansible profile folder

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+echo "#####" # empty line
 echo "Start Entrypoint ..."
 
 echo "Add variables for Azure..."
@@ -22,16 +23,21 @@ AZURE_TENANT_ID=${AZURE_TENANT_ID}
 export AZURE_SECRET=${AZURE_CLIENT_SECRET}
 export AZURE_TENANT=${AZURE_TENANT_ID}
 
+
 # Check if az extensions must be installed:
 for i in ${AZURE_CLI_EXTENSIONS_TO_ENABLE}
 do
+    echo "#####" # empty line
     echo "Add az extension '$i' ..."
     az extension add --name $i
 done
 
 # Upgrade BICEP and Azure CLI
+echo "#####" # empty line
 echo "Upgrade and install Azure bicep..." && az bicep upgrade
+echo "#####" # empty line
 echo "Upgrade Azure CLI..." && az upgrade
+echo "#####" # empty line
 
 # Check if i can login to azure cli
 if [ -n "${AZURE_CLIENT_ID}"  ] && [ -n "${AZURE_CLIENT_SECRET}"  ] && [ -n "${AZURE_TENANT_ID}"  ] 
@@ -48,9 +54,21 @@ else
 fi
 
 # Install requirements file if exists in ~/requirements.yml
+echo "#####" # empty line
 echo "Check if additional galaxies must be installed from ~/requirements.yml or ~/ansible/requirements.yml"
 [ -f ~/requirements.yml ] && ansible-galaxy install -r ~/requirements.yml
 [ -f ~/ansible/requirements.yml ] && ansible-galaxy install -r ~/ansible/requirements.yml
+
+# Create Symlink for .azure folder if not exist
+echo "#####" # empty line
+echo "Check symlink for .azure folder..."
+[ -d /tmp/semaphore/.azure ] || ln -s /home/semaphore/.azure /tmp/semaphore/
+
+# Create Symlink for .ansible folder if not exist
+echo "#####" # empty line
+echo "Check symlink for .ansible folder..."
+[ -d /tmp/semaphore/.ansible ] || ln -s /home/semaphore/.ansible /tmp/semaphore/
+
 
 # Start the following command:
 echo "#####" # empty line
